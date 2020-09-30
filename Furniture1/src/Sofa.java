6796f4695770871e8c1ebbf2305bd5ae5efe7ee5
sarkisyanlilit <70714938+sarkisyanlilit@users.noreply.github.com>
2020-10-01 01:53:29 +0300

    Delete Sofa.java

--- Furniture1/src/Sofa.java
+++
@@ -1,9 +0,0 @@
-package furnitures;
-public class Sofa extends Furniture {
-    Sofa(){
-        color="Бежевый";
-        name="диван";
-        cost=30000;
-
-    }public Sofa(int cost){setCost(cost);}
-}
