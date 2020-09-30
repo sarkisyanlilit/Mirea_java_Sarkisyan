78454bc1e5b6f9888da37568bd41708948252514
sarkisyanlilit <70714938+sarkisyanlilit@users.noreply.github.com>
2020-10-01 02:04:55 +0300

    Delete TestDog.java

--- dog/src/TestDog.java
+++
@@ -1,9 +0,0 @@
-
-public class TestDog {
-    public static void main(String[] agr){
-        Husky h=new Husky();
-         Labrador l=new Labrador();
-        System.out.println(h);
-        System.out.println(l);
-    }
-}
