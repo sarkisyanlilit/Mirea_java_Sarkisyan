4e657631ea9a8798d20235f4aa6c384d34039a40
sarkisyanlilit <70714938+sarkisyanlilit@users.noreply.github.com>
2020-10-01 01:57:06 +0300

    Add files via upload

---
+++ practic5-6/src/Table.java
@@ -0,0 +1,10 @@
+package furnitures;
+public class Table extends Furniture {
+    Table(){
+        color="Черный";
+        name="обеденный стол";
+        cost=26000;
+
+    }
+    public Table(int cost){setCost(cost);}
+}
---
+++ practic5-6/src/TestFurniture.java
@@ -0,0 +1,9 @@
+package furnitures;
+
+public class TestFurniture {
+    public static void main(String[] args) {
+        FurnitureShop furshop = new FurnitureShop();
+        furshop.Catalog();
+
+    }
+}
